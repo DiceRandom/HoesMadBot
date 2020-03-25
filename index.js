357f4d69a7ad245ebff04e7b4267c38aad6b0eb9
Dice A <51027869+DiceRandom@users.noreply.github.com>
2020-03-25 18:11:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const Discord = require("discord.js");
 const auth = require("./auth.json");
 const infoJson = require("./package.json");
@@ -8,88 +7,83 @@ const token = auth.token;
 var currentConnectedServers = 0; // 0 byt default
 var work = true;
 
-bot.on('ready', () =>{
-	work = true;
-	console.log("Bot Online.");
+bot.on('ready', () => {
+    work = true;
+    console.log("Bot Online.");
     getCurrentServers();
     setCurrentServers();
 })
 
-bot.on('message', message=>{
-	let args = message.content.substring(prefix.length).split(" ");
-	
+bot.on('message', message => {
+    let args = message.content.substring(prefix.length).split(" ");
 
-	if (message.author == bot.user) { // Prevent bot from responding to its own messages
+
+    if (message.author == bot.user) { // Prevent bot from responding to its own messages
         return
     }
 
-	arrayOfMessages = ['fuck you', "fuck me" ,"hoes mad", "fuck all of you"];    
+    arrayOfMessages = ['fuck you', "fuck me", "hoes mad", "fuck all of you"];
 
     //if(message.author.role.name == "Owner"){
     //	message.reply(" WOAH TOUGH GUY");
     //}
 
-    
-
-
-    if(work){
-    if(message.content != null){
-	    for(var i = 0; i< arrayOfMessages.length; i++){
-	    	if(message.content.toLowerCase().includes(arrayOfMessages[i])){
-	    		message.channel.send(new Discord.Attachment("https://i.ytimg.com/vi/Go1H4jB8Baw/hqdefault.jpg"));
-				console.log("=  someone used a used a message")
-				setCurrentServers();
-	    	}
-	    }
-	}
-	}
-
-    // don't mind this i was too lazy to make a better system
-	
 
 
 
-	if(message.content.startsWith(prefix)){
-	switch(args[0]){
-		case 'info':
-			if(args[1] === "version"){
-				message.channel.send(package.version);
-				console.log("= someone asked about version")
-			}else if(args[1] === "creator"){
-				message.channel.send("@ran.dice#9734");
-				console.log("= someone asked about you")
-			}
-			break;
-		case 'stop':
-			work = false;
-			message.channel.send(" Hoes disabled");
-			console.log("= someone disabled your hoes")
-		break;
+    if (work) {
+        if (message.content != null) {
+            for (var i = 0; i < arrayOfMessages.length; i++) {
+                if (message.content.toLowerCase().includes(arrayOfMessages[i])) {
+                    message.channel.send(new Discord.Attachment("https://i.ytimg.com/vi/Go1H4jB8Baw/hqdefault.jpg"));
+                    console.log("=  someone used a used a message")
+                    setCurrentServers();
+                }
+            }
+        }
+    }
 
-		case 'start':
-			work = true;
-			console.log("= someone enabled your hoes :)")
-		break;
-	
-	}
-}
+    if (message.content.startsWith(prefix)) {
+        switch (args[0]) {
+            case 'info':
+                if (args[1] === "version") {
+                    message.channel.send(package.version);
+                    console.log("= someone asked about version")
+                } else if (args[1] === "creator") {
+                    message.channel.send("@ran.dice#9734");
+                    console.log("= someone asked about you")
+                }
+                break;
+            case 'stop':
+                work = false;
+                message.channel.send(" Hoes disabled");
+                console.log("= someone disabled your hoes")
+                break;
+
+            case 'start':
+                work = true;
+                console.log("= someone enabled your hoes :)")
+                break;
+
+        }
+    }
 })
 
 function setCurrentServers() {
-		getCurrentServers()
-		if(currentConnectedServers = 1){
-    		bot.user.setActivity("with hoes that are mad on a server.")
-		}else if(currentConnectedServers > 1){
-			bot.user.setActivity("with hoes that are mad on " + currentConnectedServers + " servers!")
-		}else{
-			bot.user.setActivity("Nobody's mad")
-		}
+    getCurrentServers()
+    if (currentConnectedServers = 1) {
+        bot.user.setActivity("with hoes that are mad on a server.")
+    } else if (currentConnectedServers > 1) {
+        bot.user.setActivity("with hoes that are mad on " + currentConnectedServers + " servers!")
+    } else {
+        bot.user.setActivity("Nobody's mad")
+    }
 }
 
-function getCurrentServers(){
-	currentConnectedServers = 0;
-	console.log("Servers:")
-	bot.guilds.forEach((guild) => {
+function getCurrentServers() {
+    currentConnectedServers = 0;
+    console.log("Servers:")
+    bot.guilds.forEach((guild) => {
         console.log(" - " + guild.name)
         currentConnectedServers++;
     })
